9d125413cc594e3332b78fdb03f4288401bc20da
atulpratapsinghwebreinvent <atul-s001@webreinvent.com>
2024-07-26 16:09:33 +0530

    Passing the Data

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $arrayCode = ['This','is the','Atul Code'];
+    return view('welcome',compact('arrayCode'));
 });
